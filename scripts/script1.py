3c1b5b2330dfdb7fac636510cebd459ccfc291db
Igor Shusterman <ishusterman2011@gmail.com>
2025-01-15 11:05:15 +0200

    update

--- scripts/script1.py
+++ scripts/script1.py
@@ -1,8 +1,6 @@
 import pandas as pd
 file_5 = r"C:\Users\geosimlab\Documents\Igor\experiments\Gesher-5\stat_5.csv"
 file_8 = r"C:\Users\geosimlab\Documents\Igor\experiments\Gesher-6\stat_6.csv"
-#file_5 = r"C:\Users\geosimlab\Documents\Igor\experiments\LeoBek-10\stat_1.csv"
-#file_8 = r"C:\Users\geosimlab\Documents\Igor\experiments\LeoBek-10\stat-2.csv"
 df_5 = pd.read_csv(file_5)
 df_8 = pd.read_csv(file_8)
 filtered_df_8 = df_8[df_8['Destination_ID'].isin(df_5['Destination_ID'])]
